da8ec33364c58a16797988b2f5c530e1aed5c082
Jovan Teo <jovanty61@gmail.com>
2025-09-23 16:26:15 +0800

    Tutorial-2: Add remark command

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -29,6 +29,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private HBox cardPane;
     @FXML
+    private Label remark;
+    @FXML
     private Label name;
     @FXML
     private Label id;
@@ -55,5 +57,6 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        remark.setText(person.getRemark().value);
     }
 }
